51e8c6cdaebb1cdf5493f5a0af302885c611ba87
zach <zacharaya@gmail.com>
2025-09-16 13:02:54 -0700

    stuff2

---
+++ src/main/java/frc/robot/subsystems/algaetestintake.java
@@ -0,0 +1,8 @@
+public class algaetestintake {
+    //this is a test commit to the github leave it alone pretty please
+    public class AlgaeTestIntake {
+     rotationMotor;
+        intakeMotor;    
+        piston;   
+    }
+}
